e9e13f9bb2aea131610251912e27f6dd03f1df2a
Tedydev <Hieudat2310.bh@gmail.com>
2024-10-18 11:49:40 +0700

    update link logo

--- src/components/canvas/Canvas.jsx
+++ src/components/canvas/Canvas.jsx
@@ -58,7 +58,7 @@ const Canvas = ({ bladeMode = '', ofCanvasArea }) => {
 				<div className="offcanvas__body">
 					<div className="offcanvas__left">
 						<div className="offcanvas__logo">
-							<Link href="/digital-marketing">
+							<Link href="https://nphdigital.com/">
 								<Image
 									priority
 									style={{ width: '200px', height: 'auto' }}
